84b3747db4e934e4bbec23bd63b416e96fdfdf7d
Aline Freitas <148965353+linefs@users.noreply.github.com>
2023-12-20 13:33:05 -0300

    Add files via upload

---
+++ ContaController.java
@@ -0,0 +1,72 @@
+package conta.controller;
+
+import java.util.ArrayList;
+import conta.model.Conta;
+import conta.repository.ContaRepository;
+
+public class ContaController implements ContaRepository {
+	
+	
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+	int numero = 0;
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodas() {
+		// TODO Auto-generated method stub
+		for (var conta : listaContas) {
+			conta.visualizar();
+		}
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		// TODO Auto-generated method stub
+		listaContas.add(conta);
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	
+
+	public int gerarNumero() {
+		// TODO Auto-generated method stub
+		return ++ numero;
+	}
+
+}
